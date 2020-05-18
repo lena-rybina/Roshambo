50be3b77ad79617b7d429b5185921e2cd90dd3e3
lena-rybina <ielena.rybina@gmail.com>
2020-05-18 15:52:42 -0400

    Result View Controller

--- Roshambo/ResultViewController.swift
+++ Roshambo/ResultViewController.swift
@@ -6,16 +6,49 @@
 //  Copyright © 2020 Elena Rybina. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import Foundation
 
 class ResultViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultImage: UIImageView!
 
+    var gameState: GameState? = nil
+    var gameCheck: (userInput: GameSelectionType, computerInput: GameSelectionType)? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switch gameState {
+        case .win:
+            resultLabel.text = "You won!"
+        case .lose:
+            resultLabel.text = "You lost!"
+        case .tie:
+            resultLabel.text = "It's a tie!"
+        default:
+            print("[DEBUG] not handled")
+        }
+        
+        switch gameCheck! {
+        case (.rock, .scissors),
+             (.scissors, .rock):
+            return resultImage.image = UIImage(named: "RockCrushesScissors")
+        case (.rock, .paper),
+             (.paper, .rock):
+            return resultImage.image = UIImage(named: "PaperCoversRock")
+        case (.paper, .scissors),
+         (.scissors, .paper):
+            return resultImage.image = UIImage(named: "ScissorsCutPaper")
+        default:
+            resultImage.image = UIImage(named: "itsATie")
+        }
     }
-
-
+    
+    @IBAction func dismiss() {
+        // dismiss this view controller
+        self.dismiss(animated: true, completion: nil)
+    }
 }
+
 
--- Roshambo/SelectViewController.swift
+++ Roshambo/SelectViewController.swift
@@ -10,16 +10,54 @@ import Foundation
 import UIKit
 
 class SelectViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    
+    var gameState: GameState?
+    
+    var userInput: GameSelectionType?
+    var computerInput: GameSelectionType?
+    
+    @IBAction func onButtonTap(_ sender: UIButton) {
         let allCases = GameSelectionType.allCases
-        allCases.randomElement()
         
-        print("Is game working - \(GameEngine.gameCheck(userInput: .rock, computerInput: .paper))")
+        let computerInput = allCases.randomElement()!
+        
+        var userInput: GameSelectionType
+        
+        switch sender {
+        case let button where button == rockButton:
+            userInput = .rock
+        case let button where button == paperButton:
+            userInput = .paper
+        case let button where button == scissorsButton:
+            userInput = .scissors
+        default:
+            userInput = .scissors
+        }
+        
+        self.userInput = userInput
+        self.computerInput = computerInput
+        
+        self.gameState = GameEngine.gameCheck(userInput: userInput,
+                                              computerInput: computerInput)
+        
+        performSegue(withIdentifier: "resultSegue",
+                     sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "resultSegue",
+            let controller = segue.destination as? ResultViewController {
+            
+            controller.gameState = gameState
+            
+            guard let userInput = userInput,
+                let computerInput = computerInput else { return }
+            controller.gameCheck = (userInput,
+                                    computerInput)
+        
+        }
     }
 }
-
-    
